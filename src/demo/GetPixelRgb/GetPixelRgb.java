ba42416a1bc78880ad707f17dcd1e2359616c0fd
Cheng-Yu <e88885@gmail.com>
2017-01-10 13:10:18 +0800

    更新註解

--- src/demo/GetPixelRgb/GetPixelRgb.java
+++ src/demo/GetPixelRgb/GetPixelRgb.java
@@ -1,23 +1,14 @@
 package demo.GetPixelRgb;
 
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.provider.MediaStore.Images.Media;
-import android.util.DisplayMetrics;
 import android.util.Log;
 
-import java.io.FileNotFoundException;
-
 import android.app.Activity;
-import android.content.ContentResolver;
-import android.content.ContentValues;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
@@ -36,8 +27,7 @@ public class GetPixelRgb extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);		
-		
-//		String rgbString[] = {"Red: ", "Green: ", "Blue: "};
+
 		String rgbString[] = {"R: ", "G: ", "B: "};
 		colorTextView = new TextView[colorTextViewId.length];
 		for(int i=0; i<colorTextViewId.length; i++){
@@ -50,14 +40,15 @@ public class GetPixelRgb extends Activity {
 
 		imgSource1.setOnTouchListener(imgSourceOnTouchListener);
 		imgSource1.setImageResource(R.drawable.peoples);
-
 	}
 	
+	// 啟動相機按鈕事件
 	public void cameraOnClick(View view) {
 		Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE); 
         startActivityForResult(cameraIntent, CAMERA_REQUEST); 
 	}	
 	
+	// 接收相機拍攝的影像
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {  
         if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {  
             Bitmap photo = (Bitmap) data.getExtras().get("data"); 
@@ -65,79 +56,87 @@ public class GetPixelRgb extends Activity {
         }  
     }
 	
-	OnTouchListener imgSourceOnTouchListener = new OnTouchListener() {
+	// 點擊觸螢處發事件
+	private OnTouchListener imgSourceOnTouchListener = new OnTouchListener() {
 
 		@Override
 		public boolean onTouch(View view, MotionEvent event) {
-
-			float eventX = event.getX();
-			float eventY = event.getY();
-			float[] eventXY = new float[] {eventX, eventY};
-
-			Matrix invertMatrix = new Matrix();
-			((ImageView)view).getImageMatrix().invert(invertMatrix);
-
-			invertMatrix.mapPoints(eventXY);
-			int x = Integer.valueOf((int)eventXY[0]);
-			int y = Integer.valueOf((int)eventXY[1]);
 			
-			
-			Log.d("debug", 
-					"Touched position: \n"
-				    + String.valueOf(eventX) + " / "
-				    + String.valueOf(eventY));
+		    switch (event.getAction()) {
+		    case MotionEvent.ACTION_DOWN:
+		    	// 當接觸螢幕未放開時		    	
+		        break;		   
+		    case MotionEvent.ACTION_UP:
+		    	// 當接觸螢幕已放開時
+		    	view.performClick();
+		        break;
+		    default:
+		    	// 
+		    	float eventX = event.getX();
+				float eventY = event.getY();
+				float[] eventXY = new float[] {eventX, eventY};
 
-			Log.d("debug", 
-				    "Touched position: \n"
-				    + String.valueOf(x) + " / "
-				    + String.valueOf(y));
+				Matrix invertMatrix = new Matrix();
+				((ImageView)view).getImageMatrix().invert(invertMatrix);
 
-			Drawable imgDrawable = ((ImageView)view).getDrawable();
-			Bitmap bitmap = ((BitmapDrawable)imgDrawable).getBitmap();
+				invertMatrix.mapPoints(eventXY);
+				int x = Integer.valueOf((int)eventXY[0]);
+				int y = Integer.valueOf((int)eventXY[1]);
+				
+				Log.d("debug", 
+						"Touched position: \n"
+					    + String.valueOf(eventX) + " / "
+					    + String.valueOf(eventY));
 
-			Log.d("debug", 
-				    "Drawable size: \n"
-				    + String.valueOf(bitmap.getWidth()) + " / "
-				    + String.valueOf(bitmap.getHeight()));
+				Log.d("debug", 
+					    "Touched position: \n"
+					    + String.valueOf(x) + " / "
+					    + String.valueOf(y));
 
-			//Limit x, y range within bitmap
-			if(x < 0) {
-				x = 0;
-			} else if(x > bitmap.getWidth()-1) {
-				x = bitmap.getWidth()-1;
-			}
+				Drawable imgDrawable = ((ImageView)view).getDrawable();
+				Bitmap bitmap = ((BitmapDrawable)imgDrawable).getBitmap();
 
-			if(y < 0) {
-				y = 0;
-			} else if(y > bitmap.getHeight()-1) {
-				y = bitmap.getHeight()-1;
-			}
+				Log.d("debug", 
+					    "Drawable size: \n"
+					    + String.valueOf(bitmap.getWidth()) + " / "
+					    + String.valueOf(bitmap.getHeight()));
 
-			int touchedRGB = bitmap.getPixel(x, y);
-			
-			int red 	= (touchedRGB>>16) & 0x0ff;
-			int green	= (touchedRGB>>8)  & 0x0ff;
-			int blue	= (touchedRGB)     & 0x0ff;
-			
-			Log.d("debug",
-					"R: " + red + ", " +
-					"G: " + green + ", " +
-					"B: " + blue);
+				//Limit x, y range within bitmap
+				if(x < 0) {
+					x = 0;
+				} else if(x > bitmap.getWidth()-1) {
+					x = bitmap.getWidth()-1;
+				}
 
-//			colorTextView[0].setText("Red: " + red);
-//			colorTextView[1].setText("Green: " + green);
-//			colorTextView[2].setText("Blue: " + blue);
-			
-			colorTextView[0].setText("R: " + red);
-			colorTextView[1].setText("G: " + green);
-			colorTextView[2].setText("B: " + blue);
-			
-			imgSource2.setBackgroundColor(touchedRGB);
-//			colorRGB.setText("Touched color: \n" + "#" + Integer.toHexString(touchedRGB));
-//			colorRGB.setText("r" + red + " g" + green + " b" + blue);
-//			colorRGB.setTextColor(touchedRGB);
+				if(y < 0) {
+					y = 0;
+				} else if(y > bitmap.getHeight()-1) {
+					y = bitmap.getHeight()-1;
+				}
 
-			return true;
+				int touchedRGB = bitmap.getPixel(x, y);
+				
+				int red 	= (touchedRGB>>16) & 0x0ff;
+				int green	= (touchedRGB>>8)  & 0x0ff;
+				int blue	= (touchedRGB)     & 0x0ff;
+				
+				Log.d("debug",
+						"R: " + red + ", " +
+						"G: " + green + ", " +
+						"B: " + blue);
+				
+				colorTextView[0].setText("R: " + red);
+				colorTextView[1].setText("G: " + green);
+				colorTextView[2].setText("B: " + blue);
+				
+				imgSource2.setBackgroundColor(touchedRGB);
+//				colorRGB.setText("Touched color: \n" + "#" + Integer.toHexString(touchedRGB));
+//				colorRGB.setText("r" + red + " g" + green + " b" + blue);
+//				colorRGB.setTextColor(touchedRGB);
+				
+		        break;
+		    }
+		    return true;
 		}
 	};
 
